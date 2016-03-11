540bcc4163c0be8497ad1e09af3528d7b8936efa
Wilson <xueminghao2013@gmail.com>
2016-03-11 19:00:46 +0800

    【commit】commit

---
+++ Tutorials/Tutorials.build/Debug/tutorial18_billboards.build/Objects-normal/x86_64/texture.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Users/administrator/Downloads/ogl-OpenGL-tutorial_0015_21-2/common/texture.cpp \
+  /Users/administrator/Downloads/ogl-OpenGL-tutorial_0015_21-2/external/glew-1.13.0/include/GL/glew.h \
+  /Users/administrator/Downloads/ogl-OpenGL-tutorial_0015_21-2/external/glfw-3.1.2/include/GLFW/glfw3.h
